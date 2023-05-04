dfa8b066453cd813f35c3439146a43b38830b575
vijilakunnekkat <vijilakunnekkat@gmail.com>
2023-05-04 14:29:01 +0530

    forth commit

---
+++ movieapp/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.contrib.admin import views
+from django.urls import path, include
+from . import views
+app_name='movieapp'
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',views.index,name='index'),
+    path('movie/<int:movie_id>/',views.detail,name='detail'),
+    path('add/',views.add_movie,name='add_movie'),
+    path('update/<int:id>/',views.update,name='update'),
+    path('delete/<int:id>/',views.delete,name="delete")
+]
